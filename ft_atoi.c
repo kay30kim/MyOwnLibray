ac12248331e4d494096f31d9fb72cd3b72353f8f
kay30kim <https://github.com/kay30kim>
2023-10-17 13:51:00 -0400

    add ft_atoid.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -10,4 +10,34 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+void	ft_pass(char *str, int *i, int *sign)
+{
+	while (str[*i] && ((9 <= str[*i] && str[*i] <= 13) || str[*i] == 32))
+	{
+		*i = *i + 1;
+	}
+	while (str[*i] != '\0' && (str[*i] == '+' || str[*i] == '-'))
+	{
+		if (str[*i] == '-')
+			*sign *= -1;
+		*i += 1;
+	}
+}
 
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	sign;
+	int	num;
+
+	i = 0;
+	sign = 1;
+	num = 0;
+	ft_pass(str, &i, &sign);
+	while (str[i] && '0' <= str[i] && str[i] <= '9')
+	{
+		num = num * 10 + (str[i] - '0');
+		i++;
+	}
+	return (sign * num);
+}
